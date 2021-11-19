e272d7521d328ad5e002c69a20bad374f4a4bb0b
Shihab Khan <khanshi@hhsc.ca>
2021-11-19 17:34:16 -0500

    add test resources for TestPractitionerResourceHandler and update tests

--- SanteDB.Messaging.FHIR.Test/TestPractitionerResourceHandler.cs
+++ SanteDB.Messaging.FHIR.Test/TestPractitionerResourceHandler.cs
@@ -23,8 +23,6 @@ using FirebirdSql.Data.FirebirdClient;
 using Hl7.Fhir.Model;
 using NUnit.Framework;
 using SanteDB.Core;
-using SanteDB.Core.Configuration;
-using SanteDB.Core.Model.Entities;
 using SanteDB.Core.Security;
 using SanteDB.Core.Services;
 using SanteDB.Core.TestFramework;
@@ -37,8 +35,6 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using SanteDB.Core.Model.Roles;
-using Patient = SanteDB.Core.Model.Roles.Patient;
-using Person = SanteDB.Core.Model.Entities.Person;
 
 namespace SanteDB.Messaging.FHIR.Test
 {
@@ -49,13 +45,7 @@ namespace SanteDB.Messaging.FHIR.Test
     {
         private readonly byte[] AUTH = {0x01, 0x02, 0x03, 0x04, 0x05};
 
-        private IRepositoryService<Patient> m_patientRepository;
-
-        private IRepositoryService<Person> m_personRepository;
-
         private IRepositoryService<Provider> m_providerRepositoryService;
-
-        private IRepositoryService<EntityRelationship> m_relationshipRepository;
 
         // Bundler 
         private IServiceManager m_serviceManager;
@@ -67,6 +57,7 @@ namespace SanteDB.Messaging.FHIR.Test
             var p = FbCharset.Ascii;
             TestApplicationContext.TestAssembly = typeof(TestRelatedPersonResourceHandler).Assembly;
             TestApplicationContext.Initialize(TestContext.CurrentContext.TestDirectory);
+
             this.m_serviceManager = ApplicationServiceContext.Current.GetService<IServiceManager>();
 
             var testConfiguration = new FhirServiceConfigurationSection
@@ -75,13 +66,7 @@ namespace SanteDB.Messaging.FHIR.Test
                 {
                     "Practitioner"
                 },
-                OperationHandlers = new List<TypeReferenceConfiguration>(),
-                ExtensionHandlers = new List<TypeReferenceConfiguration>(),
-                ProfileHandlers = new List<TypeReferenceConfiguration>(),
-                MessageHandlers = new List<TypeReferenceConfiguration>
-                {
-                    new TypeReferenceConfiguration(typeof(PractitionerResourceHandler))
-                }
+
             };
 
             using (AuthenticationContext.EnterSystemContext())
@@ -91,30 +76,15 @@ namespace SanteDB.Messaging.FHIR.Test
             }
         }
 
-        
+        /// <summary>
+        /// Tests the delete functionality in <see cref="PractitionerResourceHandler"/> class.
+        /// </summary>
         [Test]
         public void TestDeletePractitioner()
         {
-            //set up a practitioner for test
-            var practitioner = new Practitioner();
 
-            practitioner.Name.Add(new HumanName
-            {
-                Given = new List<string>
-                {
-                    "Test"
-                },
-                Family = "Practitioner"
-            });
-
-            practitioner.BirthDate = DateTime.Now.ToString("yyyy-MM-dd");
-            practitioner.Gender = AdministrativeGender.Male;
-            practitioner.Telecom = new List<ContactPoint>
-            {
-                new ContactPoint(ContactPoint.ContactPointSystem.Fax, ContactPoint.ContactPointUse.Work, "905 555 1234")
-            };
-
-            Resource result;
+            //load the practitioner for delete
+            var practitioner = TestUtil.GetFhirMessage("DeletePractitioner") as Practitioner;
 
             TestUtil.CreateAuthority("TEST", "1.2.3.4", "http://santedb.org/fhir/test", "TEST_HARNESS", this.AUTH);
             
@@ -124,7 +94,7 @@ namespace SanteDB.Messaging.FHIR.Test
                 var practitionerResourceHandler = FhirResourceHandlerUtil.GetResourceHandler(ResourceType.Practitioner);
 
                 // create the practitioner using the resource handler
-                result = practitionerResourceHandler.Create(practitioner, TransactionMode.Commit);
+                Resource result = practitionerResourceHandler.Create(practitioner, TransactionMode.Commit);
 
                 // retrieve the practitioner using the resource handler
                 result = practitionerResourceHandler.Read(result.Id, result.VersionId);
@@ -148,7 +118,9 @@ namespace SanteDB.Messaging.FHIR.Test
 
         }
 
-
+        /// <summary>
+        /// Tests the get interactions functionality in <see cref="PractitionerResourceHandler"/> class.
+        /// </summary>
         [Test]
         public void TestGetInteractions()
         {
@@ -158,45 +130,21 @@ namespace SanteDB.Messaging.FHIR.Test
             var interactions = methodInfo.Invoke(practitionerResourceHandler, null);
 
             Assert.True(interactions is IEnumerable<CapabilityStatement.ResourceInteractionComponent>);
-            var resouceInteractionComponents = (IEnumerable<CapabilityStatement.ResourceInteractionComponent>)interactions;
-            Assert.AreEqual(7, resouceInteractionComponents.Count());
-            Assert.IsTrue(resouceInteractionComponents.Any(c => c.Code == CapabilityStatement.TypeRestfulInteraction.Create));
+            var resourceInteractionComponents = (IEnumerable<CapabilityStatement.ResourceInteractionComponent>)interactions;
+            Assert.AreEqual(7, resourceInteractionComponents.Count());
+            Assert.IsTrue(resourceInteractionComponents.Any(c => c.Code == CapabilityStatement.TypeRestfulInteraction.Create));
 
         }
 
+        /// <summary>
+        /// Tests the create functionality in <see cref="PractitionerResourceHandler"/> class.
+        /// </summary>
         [Test]
         public void TestCreatePractitioner()
         {
-            // set up a practitioner for test
-            var practitioner = new Practitioner();
 
-            practitioner.Name.Add(new HumanName
-            {
-                Given = new List<string>
-                {
-                    "Test"
-                },
-                Family = "Practitioner"
-            });
-
-            //we need to supply 5 character code for communication, otherwise firebird test db will add trailing spaces
-            practitioner.Communication.Add(new CodeableConcept("http://tools.ietf.org/html/bcp47", "en-US"));
-            practitioner.Communication.Add(new CodeableConcept("http://tools.ietf.org/html/bcp47", "fr-CA"));
-            
-
-            practitioner.Extension.Add(new Extension("http://santedb.org/extensions/core/jpegPhoto", new Base64Binary(new byte[]{0x01, 0x02, 0x03, 0x04, 0x05})));
-            practitioner.Extension.Add(new Extension("http://santedb.org/extensions/core/detectedIssue", new Base64Binary(new byte[]{0x01, 0x02, 0x03, 0x04, 0x05})));            
-            practitioner.Identifier.Add(new Hl7.Fhir.Model.Identifier("http://santedb.org/fhir/test", "6324"));
-            practitioner.BirthDate = new DateTime(1980, 12, 1).ToString("yyyy-MM-dd");
-            practitioner.Telecom = new List<ContactPoint>
-            {
-                new ContactPoint(ContactPoint.ContactPointSystem.Fax, ContactPoint.ContactPointUse.Work, "905 555 1234")
-            };
-
-            practitioner.Photo.Add(new Attachment()
-            {
-                Data = new byte[]{0x01, 0x02, 0x03, 0x04, 0x05}
-            });
+            //load the practitioner for create
+            var practitioner = TestUtil.GetFhirMessage("CreatePractitioner") as Practitioner;
 
             Resource result;
 
@@ -222,6 +170,8 @@ namespace SanteDB.Messaging.FHIR.Test
             Assert.AreEqual(1, numOfIdentifiers);
             Assert.AreEqual("Practitioner", actual.Name.Single().Family);
             Assert.AreEqual("Test", actual.Name.Single().Given.Single());
+            //loaded practitioner must have 5 character code for communication, otherwise firebird test db will add trailing spaces
+            //and cause issues with following communication asserts
             Assert.IsTrue(actual.Communication.Any(c => c.Coding.Any(x => x.Code == "en-US")));
             Assert.IsTrue(actual.Communication.Any(c => c.Coding.Any(x => x.Code == "fr-CA")));
             Assert.IsTrue(actual.Extension.Any(e => e.Url == "http://santedb.org/extensions/core/detectedIssue"));
@@ -231,52 +181,21 @@ namespace SanteDB.Messaging.FHIR.Test
             
         }
 
+        /// <summary>
+        /// Tests the create functionality using duplicate identifiers in <see cref="PractitionerResourceHandler"/> class.
+        /// </summary>
         [Test]
-        public void TestTwoPractitionerSameIdentifier()
+        public void TestCreateTwoPractitionerSameIdentifier()
         {
-            //  set up first practitioner
-            var practitioner = new Practitioner();
+            //load the first practitioner 
+            var practitioner = TestUtil.GetFhirMessage("CreatePractitioner") as Practitioner;
 
-            practitioner.Name.Add(new HumanName
-            {
-                Given = new List<string>
-                {
-                    "First"
-                },
-                Family = "PracOne"
-            });
-
-            practitioner.Identifier.Add(new Hl7.Fhir.Model.Identifier("http://santedb.org/fhir/test", "6666"));
-            practitioner.BirthDate = DateTime.Now.ToString("yyyy-MM-dd");
-            practitioner.Gender = AdministrativeGender.Male;
-            practitioner.Telecom = new List<ContactPoint>
-            {
-                new ContactPoint(ContactPoint.ContactPointSystem.Fax, ContactPoint.ContactPointUse.Work, "905 555 1234")
-            };
-
-            //set up second practitioner
-            var secondPractitioner = new Practitioner();
-            secondPractitioner.Name.Add(new HumanName
-            {
-                Given = new List<string>
-                {
-                    "Second"
-                },
-                Family = "PracTwo"
-            });
-            //add duplicate identifier for test
-            secondPractitioner.Identifier.Add(new Hl7.Fhir.Model.Identifier("http://santedb.org/fhir/test", "6666"));
-            secondPractitioner.BirthDate = DateTime.Now.ToString("yyyy-MM-dd");
-            secondPractitioner.Gender = AdministrativeGender.Male;
-            secondPractitioner.Telecom = new List<ContactPoint>
-            {
-                new ContactPoint(ContactPoint.ContactPointSystem.Fax, ContactPoint.ContactPointUse.Work, "905 555 1234")
-            };
+            //load the second practitioner with same identifier
+            var secondPractitioner = TestUtil.GetFhirMessage("CreatePractitionerSameIdentifier") as Practitioner;
 
             Resource resultOne, resultTwo, pracOne, pracTwo;
             Practitioner actualPracOne, actualPracTwo;
 
-    
             TestUtil.CreateAuthority("TEST", "1.2.3.4", "http://santedb.org/fhir/test", "TEST_HARNESS", this.AUTH);
             
             using (TestUtil.AuthenticateFhir("TEST_HARNESS", this.AUTH))
@@ -290,8 +209,8 @@ namespace SanteDB.Messaging.FHIR.Test
                 //check if the practitioner is saved properly
                 resultOne = practitionerResourceHandler.Read(pracOne.Id, pracOne.VersionId);
                 actualPracOne = (Practitioner)resultOne;
-                Assert.AreEqual("PracOne", (actualPracOne.Name.Single().Family));
-                Assert.AreEqual("First", actualPracOne.Name.Single().Given.Single());
+                Assert.AreEqual("Practitioner", (actualPracOne.Name.Single().Family));
+                Assert.AreEqual("Test", actualPracOne.Name.Single().Given.Single());
                 Assert.NotNull(resultOne);
                 Assert.IsInstanceOf<Practitioner>(resultOne);
                 
@@ -320,48 +239,17 @@ namespace SanteDB.Messaging.FHIR.Test
 
         }
 
+        /// <summary>
+        /// Tests the create functionality using different identifiers in <see cref="PractitionerResourceHandler"/> class.
+        /// </summary>
          [Test]
-        public void TestTwoPractitionerDifferentIdentifier()
+        public void TestCreateTwoPractitionerDifferentIdentifier()
         {
-            // set up first practitioner
-            var practitioner = new Practitioner();
+            //load the first practitioner 
+            var practitioner = TestUtil.GetFhirMessage("CreatePractitioner") as Practitioner;
 
-            practitioner.Name.Add(new HumanName
-            {
-                Given = new List<string>
-                {
-                    "First"
-                },
-                Family = "PracOne"
-            });
-
-            
-            practitioner.Identifier.Add(new Hl7.Fhir.Model.Identifier("http://santedb.org/fhir/test", "1111"));
-            practitioner.BirthDate = DateTime.Now.ToString("yyyy-MM-dd");
-            practitioner.Telecom = new List<ContactPoint>
-            {
-                new ContactPoint(ContactPoint.ContactPointSystem.Fax, ContactPoint.ContactPointUse.Work, "905 555 1234")
-            };
-
-            //set up second practitioner
-            var secondPractitioner = new Practitioner();
-            secondPractitioner.Name.Add(new HumanName
-            {
-                Given = new List<string>
-                {
-                    "Second"
-                },
-                Family = "PracTwo"
-            });
-
-            //add duplicate identifier for test
-            secondPractitioner.Identifier.Add(new Hl7.Fhir.Model.Identifier("http://santedb.org/fhir/test", "2222"));
-            secondPractitioner.BirthDate = DateTime.Now.ToString("yyyy-MM-dd");
-            secondPractitioner.Gender = AdministrativeGender.Male;
-            secondPractitioner.Telecom = new List<ContactPoint>
-            {
-                new ContactPoint(ContactPoint.ContactPointSystem.Fax, ContactPoint.ContactPointUse.Work, "905 555 1234")
-            };
+            //set up second practitioner with different identifier
+            var secondPractitioner = TestUtil.GetFhirMessage("CreatePractitionerDifferentIdentifier") as Practitioner;
 
             Resource resultOne, resultTwo, pracOne, pracTwo;
             Practitioner actualPracOne, actualPracTwo;
@@ -380,8 +268,8 @@ namespace SanteDB.Messaging.FHIR.Test
                 //check if the practitioner is saved properly
                 resultOne = practitionerResourceHandler.Read(pracOne.Id, pracOne.VersionId);
                 actualPracOne = (Practitioner)resultOne;
-                Assert.AreEqual("PracOne", (actualPracOne.Name.Single().Family));
-                Assert.AreEqual("First", actualPracOne.Name.Single().Given.Single());
+                Assert.AreEqual("Practitioner", (actualPracOne.Name.Single().Family));
+                Assert.AreEqual("Test", actualPracOne.Name.Single().Given.Single());
                 Assert.NotNull(resultOne);
                 Assert.IsInstanceOf<Practitioner>(resultOne);
                 
@@ -409,34 +297,14 @@ namespace SanteDB.Messaging.FHIR.Test
 
         }
 
+        /// <summary>
+        /// Tests the read functionality in <see cref="PractitionerResourceHandler"/> class.
+        /// </summary>
         [Test]
         public void TestReadPractitioner()
         {
-            //set up a practitioner
-            var practitioner = new Practitioner();
-
-            practitioner.Name.Add(new HumanName
-            {
-                Given = new List<string>
-                {
-                    "Test"
-                },
-                Family = "Practitioner"
-            });
-
-            practitioner.Identifier.Add(new Hl7.Fhir.Model.Identifier("http://santedb.org/fhir/test", "6324"));
-            practitioner.BirthDate = DateTime.Now.ToString("yyyy-MM-dd");
-            practitioner.Address.Add(new Address()
-            {
-                City = "Hamilton",
-                Country = "Canada"
-
-            });
-            practitioner.Telecom = new List<ContactPoint>
-            {
-                new ContactPoint(ContactPoint.ContactPointSystem.Fax, ContactPoint.ContactPointUse.Work, "905 555 1234")
-            };
-
+            //load up a practitioner for read test
+            var practitioner = TestUtil.GetFhirMessage("ReadPractitioner") as Practitioner;
 
             Resource result;
 
@@ -466,27 +334,14 @@ namespace SanteDB.Messaging.FHIR.Test
             Assert.IsTrue(actual.Identifier.Any(i => i.Value == "6324"));
         }
 
+        /// <summary>
+        /// Tests the update functionality in <see cref="PractitionerResourceHandler"/> class.
+        /// </summary>
          [Test]
         public void TestUpdatePractitioner()
         {
-            //set up a practitioner
-            var practitioner = new Practitioner();
-
-            practitioner.Name.Add(new HumanName
-            {
-                Given = new List<string>
-                {
-                    "Test"
-                },
-                Family = "Patient"
-            });
-
-            practitioner.BirthDate = DateTime.Now.ToString("yyyy-MM-dd");
-            practitioner.Telecom = new List<ContactPoint>
-            {
-                new ContactPoint(ContactPoint.ContactPointSystem.Fax, ContactPoint.ContactPointUse.Work, "905 555 1234")
-            };
-
+            //load up a practitioner to update
+            var practitioner = TestUtil.GetFhirMessage("CreatePractitioner") as Practitioner;
 
             Resource result;
             Practitioner actual;
@@ -511,7 +366,7 @@ namespace SanteDB.Messaging.FHIR.Test
 
                 
                 Assert.AreEqual("Test", actual.Name.Single().Given.Single());
-                Assert.AreEqual("Patient", actual.Name.Single().Family);
+                Assert.AreEqual("Practitioner", actual.Name.Single().Family);
 
                 //update name
                 actual.Name.Clear();
@@ -539,7 +394,9 @@ namespace SanteDB.Messaging.FHIR.Test
 
         }
 
-
+        /// <summary>
+        /// Tests the create functionality using invalid resource in <see cref="PractitionerResourceHandler"/> class.
+        /// </summary>
         [Test]
         public void TestCreateInvalidResource()
         {
