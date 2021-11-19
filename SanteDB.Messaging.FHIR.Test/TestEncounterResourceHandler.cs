a28de3f76147e87c2c4514225c02484c3c84c57c
Jordan Webber <webberjo@hhsc.ca>
2021-11-19 11:25:07 -0500

    Added tests for Update and Delete with invalid resources.

--- SanteDB.Messaging.FHIR.Test/TestEncounterResourceHandler.cs
+++ SanteDB.Messaging.FHIR.Test/TestEncounterResourceHandler.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using FirebirdSql.Data.FirebirdClient;
 using Hl7.Fhir.Model;
 using NUnit.Framework;
@@ -21,6 +19,9 @@ using Person = SanteDB.Core.Model.Entities.Person;
 
 namespace SanteDB.Messaging.FHIR.Test
 {
+    /// <summary>
+    /// Test class for <see cref="EncounterResourceHandler"/>
+    /// </summary>
     class TestEncounterResourceHandler : DataTest
     {
         private readonly byte[] AUTH = { 0x01, 0x02, 0x03, 0x04, 0x05 };
@@ -34,6 +35,9 @@ namespace SanteDB.Messaging.FHIR.Test
         // Bundler 
         private IServiceManager m_serviceManager;
 
+        /// <summary>
+        /// Set up method to initialize services.
+        /// </summary>
         [SetUp]
         public void Setup()
         {
@@ -72,6 +76,9 @@ namespace SanteDB.Messaging.FHIR.Test
             }
         }
 
+        /// <summary>
+        /// Tests the create functionality in <see cref="EncounterResourceHandler"/>
+        /// </summary>
         [Test]
         public void TestCreateEncounter()
         {
@@ -100,7 +107,8 @@ namespace SanteDB.Messaging.FHIR.Test
             {
                 Class = new Coding("http://santedb.org/conceptset/v3-ActEncounterCode", "HH"),
                 Status = Encounter.EncounterStatus.Finished,
-                Subject = new ResourceReference($"urn:uuid:{patient.Id}")
+                Subject = new ResourceReference($"urn:uuid:{patient.Id}"),
+                Language = "English"
             };
 
             bundle.Entry.Add(new Bundle.EntryComponent
@@ -156,8 +164,12 @@ namespace SanteDB.Messaging.FHIR.Test
 
             Assert.AreEqual(createdEncounter.Id, actualEncounter.Id);
             Assert.AreEqual(createdEncounter.Status, actualEncounter.Status);
+            Assert.AreEqual(createdEncounter.Language, actualEncounter.Language);
         }
 
+        /// <summary>
+        /// Tests the delete functionality in <see cref="EncounterResourceHandler"/>
+        /// </summary>
         [Test]
         public void TestDeleteEncounter()
         {
@@ -242,14 +254,18 @@ namespace SanteDB.Messaging.FHIR.Test
 
                 var result = encounterResourceHandler.Delete(actualEncounter.Id, TransactionMode.Commit);
 
-                var test = encounterResourceHandler.Read(result.Id, null);
+                result = encounterResourceHandler.Read(result.Id, null);
 
-                var obsoletedEncounter = (Encounter)test;
+                var obsoletedEncounter = (Encounter)result;
+
                 Assert.AreEqual(Encounter.EncounterStatus.Unknown, obsoletedEncounter.Status);
             }
 
         }
 
+        /// <summary>
+        /// Tests the update functionality in <see cref="EncounterResourceHandler"/>
+        /// </summary>
         [Test]
         public void TestUpdateEncounter()
         {
@@ -352,6 +368,9 @@ namespace SanteDB.Messaging.FHIR.Test
             Assert.AreEqual(Encounter.EncounterStatus.Cancelled, actualEncounter.Status);
         }
 
+        /// <summary>
+        /// Tests the create method in <see cref="EncounterResourceHandler"/> confirming an invalid resource is not used.
+        /// </summary>
         [Test]
         public void TestCreateEnounterInvalidResource()
         {
@@ -361,9 +380,192 @@ namespace SanteDB.Messaging.FHIR.Test
                 // get the resource handler
                 var encounterResourceHandler = FhirResourceHandlerUtil.GetResourceHandler(ResourceType.Encounter);
 
-                // create the patient using the resource handler
+                // create the encounter using the resource handler with an incorrect resource
                 Assert.Throws<InvalidDataException>(() => encounterResourceHandler.Create(new Practitioner(), TransactionMode.Commit));
 
+            }
+        }
+
+        /// <summary>
+        /// Test updating encounter with invalid resource in <see cref="EncounterResourceHandler"/>
+        /// </summary>
+        [Test]
+        public void TestUpdateEncounterInvalidResource()
+        {
+            var bundle = new Bundle
+            {
+                Type = Bundle.BundleType.Transaction
+            };
+
+            var patient = new Hl7.Fhir.Model.Patient
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = new List<HumanName>
+                {
+                    new HumanName
+                    {
+                        Given = new List<string>
+                        {
+                            "Jordan"
+                        },
+                        Family = "Webber"
+                    }
+                }
+            };
+
+            var encounter = new Encounter()
+            {
+                Class = new Coding("http://santedb.org/conceptset/v3-ActEncounterCode", "HH"),
+                Status = Encounter.EncounterStatus.Finished,
+                Subject = new ResourceReference($"urn:uuid:{patient.Id}")
+            };
+
+            bundle.Entry.Add(new Bundle.EntryComponent
+            {
+                Resource = patient
+            });
+
+            bundle.Entry.Add(new Bundle.EntryComponent
+            {
+                Resource = encounter
+            });
+
+            Resource actual;
+
+            TestUtil.CreateAuthority("TEST", "1.2.3.4", "http://santedb.org/fhir/test", "TEST_HARNESS", AUTH);
+            using (TestUtil.AuthenticateFhir("TEST_HARNESS", AUTH))
+            {
+                var bundleResourceHandler = FhirResourceHandlerUtil.GetResourceHandler(ResourceType.Bundle);
+
+                actual = bundleResourceHandler.Create(bundle, TransactionMode.Commit);
+            }
+
+            Assert.NotNull(actual);
+
+            Assert.IsInstanceOf<Bundle>(actual);
+
+            var actualBundle = (Bundle)actual;
+
+            Assert.AreEqual(2, actualBundle.Entry.Count);
+            Assert.AreEqual(1, actualBundle.Entry.Count(c => c.Resource is Hl7.Fhir.Model.Patient));
+            Assert.AreEqual(1, actualBundle.Entry.Count(c => c.Resource is Encounter));
+
+            var createdPatient = actualBundle.Entry.Select(c => c.Resource).OfType<Hl7.Fhir.Model.Patient>().FirstOrDefault();
+
+            Assert.NotNull(createdPatient);
+
+            var createdEncounter = actualBundle.Entry.Select(c => c.Resource).OfType<Encounter>().FirstOrDefault();
+
+            Assert.NotNull(createdEncounter);
+
+            using (TestUtil.AuthenticateFhir("TEST_HARNESS", AUTH))
+            {
+                var encounterResourceHandler = FhirResourceHandlerUtil.GetResourceHandler(ResourceType.Encounter);
+
+                actual = encounterResourceHandler.Read(createdEncounter.Id, null);
+            }
+
+            Assert.IsInstanceOf<Encounter>(actual);
+
+            var retrievedEncounter = (Encounter)actual;
+
+            retrievedEncounter.Status = Encounter.EncounterStatus.Cancelled;
+            retrievedEncounter.Class = new Coding("http://santedb.org/conceptset/v3-ActEncounterCode", "HH");
+
+
+            Resource updatedEncounter;
+
+            using (TestUtil.AuthenticateFhir("TEST_HARNESS", AUTH))
+            {
+                var encounterResourceHandler = FhirResourceHandlerUtil.GetResourceHandler(ResourceType.Encounter);
+
+                Assert.Throws<InvalidDataException>(() => updatedEncounter = encounterResourceHandler.Update(retrievedEncounter.Id, new Account(), TransactionMode.Commit));
+            }
+        }
+
+        [Test]
+        public void TestDeleteEncounterInvalidGuid()
+        {
+            var bundle = new Bundle
+            {
+                Type = Bundle.BundleType.Transaction
+            };
+
+            var patient = new Hl7.Fhir.Model.Patient
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = new List<HumanName>
+                {
+                    new HumanName
+                    {
+                        Given = new List<string>
+                        {
+                            "Jordan",
+                            "Final"
+                        },
+                        Family = "Webber"
+                    }
+                }
+            };
+
+            var encounter = new Encounter()
+            {
+                Class = new Coding("http://santedb.org/conceptset/v3-ActEncounterCode", "HH"),
+                Status = Encounter.EncounterStatus.Finished,
+                Subject = new ResourceReference($"urn:uuid:{patient.Id}")
+            };
+
+            bundle.Entry.Add(new Bundle.EntryComponent
+            {
+                Resource = patient
+            });
+
+            bundle.Entry.Add(new Bundle.EntryComponent
+            {
+                Resource = encounter
+            });
+
+            Resource actual;
+
+            TestUtil.CreateAuthority("TEST", "1.2.3.4", "http://santedb.org/fhir/test", "TEST_HARNESS", AUTH);
+            using (TestUtil.AuthenticateFhir("TEST_HARNESS", AUTH))
+            {
+                var bundleResourceHandler = FhirResourceHandlerUtil.GetResourceHandler(ResourceType.Bundle);
+
+                actual = bundleResourceHandler.Create(bundle, TransactionMode.Commit);
+            }
+
+            Assert.NotNull(actual);
+
+            Assert.IsInstanceOf<Bundle>(actual);
+
+            var actualBundle = (Bundle)actual;
+
+            Assert.AreEqual(2, actualBundle.Entry.Count);
+            Assert.AreEqual(1, actualBundle.Entry.Count(c => c.Resource is Hl7.Fhir.Model.Patient));
+            Assert.AreEqual(1, actualBundle.Entry.Count(c => c.Resource is Encounter));
+
+            var createdPatient = actualBundle.Entry.Select(c => c.Resource).OfType<Hl7.Fhir.Model.Patient>().FirstOrDefault();
+
+            Assert.NotNull(createdPatient);
+
+            var createdEncounter = actualBundle.Entry.Select(c => c.Resource).OfType<Encounter>().FirstOrDefault();
+
+            Assert.NotNull(createdEncounter);
+
+            using (TestUtil.AuthenticateFhir("TEST_HARNESS", AUTH))
+            {
+                var encounterResourceHandler = FhirResourceHandlerUtil.GetResourceHandler(ResourceType.Encounter);
+
+                actual = encounterResourceHandler.Read(createdEncounter.Id, null);
+
+                Assert.NotNull(actual);
+
+                Assert.IsInstanceOf<Encounter>(actual);
+
+                var actualEncounter = (Encounter)actual;
+
+                Assert.Throws<KeyNotFoundException>(() => encounterResourceHandler.Delete(Guid.NewGuid().ToString(), TransactionMode.Commit));
             }
         }
     }
