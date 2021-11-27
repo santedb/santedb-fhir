12618eebaffd078ea77df5ffaf5d61324fe916d1
Nityan Khanna <nityan.khanna@gmail.com>
2021-11-27 00:34:23 -0500

    DSM-334

--- SanteDB.Messaging.FHIR.Test/TestFhirResourceHandlerUtil.cs
+++ SanteDB.Messaging.FHIR.Test/TestFhirResourceHandlerUtil.cs
@@ -1,16 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+ * Copyright (C) 2021 - 2021, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
+ * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors
+ * Portions Copyright (C) 2015-2018 Mohawk College of Applied Arts and Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * User: Nityan Khanna
+ * Date: 2021-11-27
+ */
+
 using Hl7.Fhir.Model;
 using NUnit.Framework;
-using SanteDB.Core;
 using SanteDB.Core.Services;
 using SanteDB.Core.TestFramework;
 using SanteDB.Messaging.FHIR.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics.CodeAnalysis;
 
 namespace SanteDB.Messaging.FHIR.Test
 {
@@ -33,6 +50,24 @@ namespace SanteDB.Messaging.FHIR.Test
         }
 
         /// <summary>
+        /// Tests the retrieval of a resource handler in the <see cref="FhirResourceHandlerUtil"/> class.
+        /// </summary>
+        [Test]
+        public void TestGetResourceHandlerNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => FhirResourceHandlerUtil.GetResourceHandler(null));
+        }
+
+        /// <summary>
+        /// Tests the retrieval of a resource handler in the <see cref="FhirResourceHandlerUtil"/> class.
+        /// </summary>
+        [Test]
+        public void TestGetResourceHandlerInvalidResource()
+        {
+            Assert.Throws<KeyNotFoundException>(() => FhirResourceHandlerUtil.GetResourceHandler("Address"));
+        }
+
+        /// <summary>
         /// Tests the registration of a FHIR resource handler in the <see cref="FhirResourceHandlerUtil"/> class.
         /// </summary>
         [Test]
@@ -42,6 +77,15 @@ namespace SanteDB.Messaging.FHIR.Test
 
             Assert.NotNull(FhirResourceHandlerUtil.GetResourceHandler(ResourceType.DomainResource));
             Assert.IsInstanceOf<DummyResourceHandler>(FhirResourceHandlerUtil.GetResourceHandler(ResourceType.DomainResource));
+        }
+
+        /// <summary>
+        /// Tests the registration of a FHIR resource handler in the <see cref="FhirResourceHandlerUtil"/> class.
+        /// </summary>
+        [Test]
+        public void TestRegisterResourceHandlerNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => FhirResourceHandlerUtil.RegisterResourceHandler(null));
         }
 
         /// <summary>
