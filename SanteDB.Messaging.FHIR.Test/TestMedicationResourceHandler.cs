dd93e7cf0270574c842279d3273df9718a1bab57
Nityan Khanna <nityan.khanna@gmail.com>
2021-12-14 10:08:51 -0500

    added documentation to the test medication resource handler

--- SanteDB.Messaging.FHIR.Test/TestMedicationResourceHandler.cs
+++ SanteDB.Messaging.FHIR.Test/TestMedicationResourceHandler.cs
@@ -1,4 +1,25 @@
-﻿using Hl7.Fhir.Model;
+﻿/*
+ * Copyright (C) 2021 - 2021, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
+ * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors
+ * Portions Copyright (C) 2015-2018 Mohawk College of Applied Arts and Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * User: khannan
+ * Date: 2021-12-1
+ */
+
+using Hl7.Fhir.Model;
 using NUnit.Framework;
 using SanteDB.Core;
 using SanteDB.Core.Configuration;
@@ -16,6 +37,9 @@ using System.Reflection;
 
 namespace SanteDB.Messaging.FHIR.Test
 {
+    /// <summary>
+    /// Contains tests for the <see cref="MedicationResourceHandler"/> class.
+    /// </summary>
     [TestFixture]
     [ExcludeFromCodeCoverage]
     public class TestMedicationResourceHandler : DataTest
@@ -36,11 +60,9 @@ namespace SanteDB.Messaging.FHIR.Test
         [SetUp]
         public void Setup()
         {
-            // Force load of the DLL
-            //var p = FbCharset.Ascii;
             TestApplicationContext.TestAssembly = typeof(TestOrganizationResourceHandler).Assembly;
             TestApplicationContext.Initialize(TestContext.CurrentContext.TestDirectory);
-            m_serviceManager = ApplicationServiceContext.Current.GetService<IServiceManager>();
+            this.m_serviceManager = ApplicationServiceContext.Current.GetService<IServiceManager>();
 
             var testConfiguration = new FhirServiceConfigurationSection
             {
