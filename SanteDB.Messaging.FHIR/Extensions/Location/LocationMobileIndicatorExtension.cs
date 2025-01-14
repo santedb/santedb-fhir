c292f8d814cefae142e31689f3e81ed810a443dd
Justin Fyfe <justin@fyfesoftware.ca>
2025-01-14 10:34:25 -0500

    Updated the BI indicator generation to support differing types of calculations

---
+++ SanteDB.Messaging.FHIR/Extensions/Location/LocationMobileIndicatorExtension.cs
@@ -0,0 +1,45 @@
+﻿using Hl7.Fhir.Model;
+using SanteDB.Core.Model;
+using SanteDB.Core.Model.Entities;
+using SanteDB.Core.Model.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SanteDB.Messaging.FHIR.Extensions.Location
+{
+    /// <summary>
+    /// FHIR extension handler for mobile locations
+    /// </summary>
+    public class LocationMobileIndicatorExtension : IFhirExtensionHandler
+    {
+        /// <inheritdoc/>
+        public Uri Uri => new Uri($"{FhirConstants.SanteDBProfile}/extension/Location/mobileIndicator");
+
+        /// <inheritdoc/>
+        public Uri ProfileUri => new Uri(FhirConstants.SanteDBProfile);
+
+        /// <inheritdoc/>
+        public ResourceType? AppliesTo => ResourceType.Location;
+
+        /// <inheritdoc/>
+        public IEnumerable<Extension> Construct(IAnnotatedResource modelObject)
+        {
+            if (modelObject is Place plc)
+            {
+                yield return new Extension(this.Uri.ToString(), new FhirBoolean(plc.IsMobile));
+            }
+        }
+
+        /// <inheritdoc/>
+        public bool Parse(Extension fhirExtension, IdentifiedData modelObject)
+        {
+            if(fhirExtension.Value is FhirBoolean fb && modelObject is Place plc)
+            {
+                plc.IsMobile = fb.Value.GetValueOrDefault();
+                return true;
+            }
+            return false;
+        }
+    }
+}
