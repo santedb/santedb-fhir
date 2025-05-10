b0794e4d2154fbcd325d4448797ff0313a758abe
Justin Fyfe <justin@fyfesoftware.ca>
2025-05-10 16:18:39 -0400

    Fixed processing of CDSS files

---
+++ SanteDB.Messaging.FHIR/Extensions/Medication/NameExtensionHandler.cs
@@ -0,0 +1,56 @@
+﻿using Hl7.Fhir.Model;
+using SanteDB.Core.Data.Quality;
+using SanteDB.Core.Model;
+using SanteDB.Core.Model.Constants;
+using SanteDB.Core.Model.Entities;
+using SanteDB.Core.Model.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SanteDB.Messaging.FHIR.Extensions.Medication
+{
+    /// <summary>
+    /// Trade name for a medication
+    /// </summary>
+    public class NameExtensionHandler : IFhirExtensionHandler
+    {
+        /// <inheritdoc/>
+        public Uri Uri => new Uri($"{FhirConstants.SanteDBProfile}/extension/Medication/name");
+
+        /// <inheritdoc/>
+        public Uri ProfileUri => new Uri(FhirConstants.SanteDBProfile);
+
+        /// <inheritdoc/>
+        public ResourceType? AppliesTo => ResourceType.Medication;
+
+        /// <inheritdoc/>
+        public IEnumerable<Extension> Construct(IAnnotatedResource modelObject)
+        {
+            if(modelObject is ManufacturedMaterial mmat)
+            {
+                // Grab the instance relationship source
+                yield return new Extension()
+                {
+                    Url = this.Uri.ToString(),
+                    Value = new FhirString(mmat.LoadProperty(o => o.Names).FirstOrDefault().ToDisplay())
+                };
+            }
+        }
+
+        /// <inheritdoc/>
+        public bool Parse(Extension fhirExtension, IdentifiedData modelObject)
+        {
+            if(fhirExtension.Value is FhirString fstr && modelObject is ManufacturedMaterial mmat)
+            {
+                mmat.Names = new List<EntityName>()
+                {
+                    new EntityName(NameUseKeys.Assigned, fstr.Value)
+                };
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ SanteDB.Messaging.FHIR/Extensions/Medication/ProductClassExtensionTypeHandler.cs
@@ -0,0 +1,47 @@
+﻿using Hl7.Fhir.Model;
+using SanteDB.Core.Model;
+using SanteDB.Core.Model.Constants;
+using SanteDB.Core.Model.Entities;
+using SanteDB.Core.Model.Interfaces;
+using SanteDB.Messaging.FHIR.Util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SanteDB.Messaging.FHIR.Extensions.Medication
+{
+    /// <summary>
+    /// Identifies the classification of the medication
+    /// </summary>
+    public class ProductClassExtensionTypeHandler : IFhirExtensionHandler
+    {
+        /// <inheritdoc/>
+        public Uri Uri => new Uri($"{FhirConstants.SanteDBProfile}/extension/Medication/classification");
+
+        /// <inheritdoc/>
+        public Uri ProfileUri => new Uri(FhirConstants.SanteDBProfile);
+
+        /// <inheritdoc/>
+        public ResourceType? AppliesTo => ResourceType.Medication;
+
+        /// <inheritdoc/>
+        public IEnumerable<Extension> Construct(IAnnotatedResource modelObject)
+        {
+            if(modelObject is Material mat)
+            {
+                yield return new Extension(this.Uri.ToString(), DataTypeConverter.ToFhirCodeableConcept(mat.DeterminerConceptKey));
+            }
+        }
+
+        /// <inheritdoc/>
+        public bool Parse(Extension fhirExtension, IdentifiedData modelObject)
+        {
+            if (modelObject is Material mat && fhirExtension.Value is CodeableConcept cc)
+            {
+                mat.DeterminerConcept = DataTypeConverter.ToConcept(cc);
+                return true;
+            }
+            return false;
+        }
+    }
+}
